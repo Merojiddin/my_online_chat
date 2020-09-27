4559edd8dba5e072cb97ffa87d79bd22f08038e3
Merojiddin <merojiddin_1998@mail.ru>
2020-09-27 18:28:48 +0800

    done totally

---
+++ my_chat/build/precache-manifest.77334c0f0dac62f143c2858c25b6eff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3b4edfba42d54d18830fa238474857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1281d12bda34b091c7f",
+    "url": "/static/css/main.ea495eb1.chunk.css"
+  },
+  {
+    "revision": "fe8888ca5c61d44520a8",
+    "url": "/static/js/2.032231df.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.032231df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1281d12bda34b091c7f",
+    "url": "/static/js/main.49fae6ba.chunk.js"
+  },
+  {
+    "revision": "b05e71e12a2bb365057d",
+    "url": "/static/js/runtime-main.91eee015.js"
+  }
+]);
